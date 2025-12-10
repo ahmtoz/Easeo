cea5c859f45e53b2d9e8e7d1bd40cab6931538aa
Ahmet Öz <Ahmet Z>
2025-12-10 17:28:05 +0300

    footer responsive layout added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,30 +2,32 @@ import './../styles/footer.css';
 
 export function Footer() {
   return (
-    <footer className="footer">
+    <footer>
         <div className="footer-container">
             <div className="header">
                 <p>AccEase</p>
             </div>
-            <div className="links">
-                <p>Useful Links</p>
-                <ul>
-                    <li><a href="#hero">Home</a></li>
-                    <li><a href="#info">Extension</a></li>
-                    <li><a href="#features">User guide</a></li>
-                    <li><a href="#contact">About us</a></li>
-                </ul>
-            </div>
-            <div>
-                <p>Contact info</p>
-                <p>Have questions? Reach out to us.</p>
-                <div>
-                    <img src="" alt="" />
-                    <span>access.ease@gmail.com</span>
+            <div className="mobile-wrapper">
+                <div className="links">
+                    <p>Useful Links</p>
+                    <ul>
+                        <li><a href="#hero">Home</a></li>
+                        <li><a href="#info">Extension</a></li>
+                        <li><a href="#features">User guide</a></li>
+                        <li><a href="#contact">About us</a></li>
+                    </ul>
                 </div>
-                <div>
-                    <img src="" alt="" />
-                    <span>Ankara / Turkey</span>
+                <div className='contact-info'>
+                    <p>Contact info</p>
+                    <p>Have questions? Reach out to us.</p>
+                    <div>
+                        <img src="" alt="" />
+                        <span>access.ease@gmail.com</span>
+                    </div>
+                    <div>
+                        <img src="" alt="" />
+                        <span>Ankara / Turkey</span>
+                    </div>
                 </div>
             </div>
         </div>
