895633bd2f23a2336746ff07add56aab8ba7af42
Ahmet Öz <Ahmet Z>
2025-12-28 23:14:51 +0300

    feat section styles updated

--- src/pages/Home/sections/FeaturesSection.jsx
+++ src/pages/Home/sections/FeaturesSection.jsx
@@ -1,34 +1,37 @@
 import AddOnElement from "../../../components/UI/AddOnElement";
 import './../../../styles/featuresSection.css';
+import Feat1 from '../../../assets/images/feat1.png';
+import Feat2 from '../../../assets/images/feat2.png';
+import Feat3 from '../../../assets/images/feat3.png';
 
 function FeaturesSection() {
   return (
     <section>
         <div className="features-container">
             <div className="features-header">
-                <h2>Look at our add - ons/extension</h2>
+                <h2>Look at our extensions</h2>
             </div>
             <div className="features-items-container">
                 <AddOnElement
-                img=""
-                altText="feature1"
+                img={Feat1}
+                altText="feature section image 1"
                 heading="Color Blindness"
-                text="Adjusts colors and contrast for clear distinction of all webpage elements."
-                btnText="Get Add-On"
+                text="Adjust colors for clear distinction of all webpage elements."
+                btnText="Get Extension"
                 />
                 <AddOnElement
-                img=""
-                altText="feature2"
+                img={Feat2}
+                altText="feature section image 2"
                 heading="Dyslexia"
-                text="Reformats text with specialized fonts and spacing for easier reading."
-                btnText="Soon"
+                text="Reformats text with specialized fonts and spacing for easy read. "
+                btnText="Coming Soon"
                 />
                 <AddOnElement
-                img=""
-                altText="feature3"
+                img={Feat3}
+                altText="feature section image 3"
                 heading="Keyboard Nav"
-                text="Enables full, mouse-free website navigation using only the keyboard."
-                btnText="Soon"
+                text="Enable full mouse-free website navigation using only keyboard. "
+                btnText="Coming Soon"
                 />
             </div>    
         </div>
