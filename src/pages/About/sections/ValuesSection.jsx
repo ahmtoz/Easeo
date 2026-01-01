775479f4bfb80d2d71d01b133a092f41d612a51e
Ahmet Öz <Ahmet Z>
2026-01-01 15:20:38 +0300

    values section added without mobile layout

--- src/pages/About/sections/ValuesSection.jsx
+++ src/pages/About/sections/ValuesSection.jsx
@@ -1,12 +1,41 @@
+import "../styles/valuesSection.css";
+import ValueOne from "../../../assets/images/value1.png";
+import ValueTwo from "../../../assets/images/value2.png";
+import ValueThree from "../../../assets/images/value3.png";
+
 function ValuesSection() {
     return (
-        <section className="values-section">
-            <h2>Our Values</h2>
-            <ul>
-                <li>Integrity</li>
-                <li>Customer Focus</li>
-                <li>Innovation</li>
-            </ul>
+        <section>
+            <div className="values-section">
+                <h2>Our Values</h2>
+                <div className="values-list">
+                    <ul>
+                        <li>
+                            <img src={ValueOne} alt="Integrity Icon" />
+                            <div className="divider"></div>
+                            <h3>Mission</h3>
+                            <p>We uphold the highest standards of integrity in all of our actions.</p>
+                        </li>
+                        <li>
+                            <img src={ValueTwo} alt="Innovation Icon" />
+                            <div className="divider"></div>
+                            <h3>Vision</h3>
+                            <p>We foster innovation to create value for our customers.</p>
+                        </li>
+                        <li>
+                            <img src={ValueThree} alt="Teamwork Icon" />
+                            <div className="divider"></div>
+                            <h3>Values</h3>
+                            <p>We work together, across boundaries, to meet the needs of our customers.</p>
+                        </li>
+                    </ul>
+                </div>
+                <p>
+                    That’s all about us.
+                    <br />
+                    Feel free to say hi! 
+                </p>
+            </div>
         </section>
     );
 }
