03f86bdcffca9e9823f5268c6f78a1bdf6e01a46
Ahmet Öz <ahmtoz@users.noreply.github.com>
2026-01-11 16:04:36 +0300

    quick fix

--- src/components/UI/AddOnElement.jsx
+++ src/components/UI/AddOnElement.jsx
@@ -2,7 +2,7 @@ function AddOnElement({img, altText, heading, text, btnText, onClick}) {
     return (
         <div className="addon-element">
             <img src={img} alt={altText} />
-            <h5>{heading}</h5>
+            <h3>{heading}</h3>
             <p>{text}</p>
             <button onClick={onClick}>{btnText}</button>
         </div>
